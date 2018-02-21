2ecc7435ba3862cfbe1f63d2b602b988d8d06a18
Berlok <10505374+berlok@users.noreply.github.com>
2018-02-21 15:43:33 +0100

    added comments

--- SampleApp/test.c
+++ SampleApp/test.c
@@ -1,7 +1,11 @@
+/* My first C programme
+hello world
+*/
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World! My name is...");
+
+    printf("Hello World! My name is..."); //print out the string in console
     return 0;
 }
